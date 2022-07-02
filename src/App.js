a6fd24024b958fd8147665b7a91d5a00188db7ad
rayhook <koohyar@gmail.com>
2022-07-02 17:51:11 +0200

    implement basic form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
-function App() {
+const App = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: 8 }}>
+      <Typography component="h2" variant="h3">
+        Greatest form
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              name="first_name"
+              fullWidth
+              label="First Name"
+              autoFocus
+            ></TextField>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField required name="last_name" fullWidth label="Last Name"></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField name="email" label="Email" fullWidth required></TextField>
+          </Grid>
+        </Grid>
+        <Button sx={{ mt: 2, mb: 3 }} fullWidth variant="contained" type="submit">
+          Submit
+        </Button>
+      </Box>
+    </Box>
   );
-}
+};
 
 export default App;
